618505e7cea8037fa6ca2c1ba9a0fa7e1ceb58fc
Ignagatti <gattiignacio85@gmail.com>
2025-08-19 13:56:39 -0300

    cambios minimos

--- Restaurante.py
+++ Restaurante.py
@@ -251,7 +251,7 @@ class Restaurante:
 
         self.ventana.configure(bg=fondo)
 
-                #dividimos
+        #dividimos
         self.parte_superior = Frame(self.ventana, bg=fondo)
         self.parte_superior.pack(fill="both", expand=True)
 
@@ -260,14 +260,17 @@ class Restaurante:
 
         self.parte_inferior.columnconfigure(0, weight=1)
         self.parte_inferior.columnconfigure(1, weight=1)
-        
 
+        #Agregamos la imagen del restaurante 
+        self.imagen = Image.open(r"Logo.png").resize((170, 180))
+        self.render = ImageTk.PhotoImage(self.imagen)
+        Label(self.parte_superior, image=self.render, bg=fondo).pack(pady=10)
 
+        Button(self.parte_inferior, text="Cerrar Sesion", width=16, font=("Arial", 12), command=lambda: self.cerrar_sesion(ventana)).pack(side="bottom", anchor="w", padx=25, pady=10)
 
-        #esta parte iria dentro de la funcion de reservar, dentro de los botones
-        Button(self.parte_inferior, text="Reservar", width=16, font=("Arial", 12), command=lambda: self.reserva(ventana)).grid(row=2, column=0, columnspan=2, padx=35, pady=5)
-
-        Button(self.parte_inferior, text="Cerrar Sesion", width=16, font=("Arial", 12), command=lambda: self.cerrar_sesion(ventana)).grid(row=8, column=0, columnspan=4, padx=25, pady=5)
+        if self.tipo == "Administrador":
+            #Agregar funciones que haga el admin, esto solo le aparece al administrador (ordenar)
+            Button(self.parte_inferior, text="Agregar Mesa", width=16,font=("Arial", 12)).pack(side="bottom", anchor="n", padx=25, pady=10)
 
     def reserva(self,ventana):
         self.ventana.destroy()
@@ -299,7 +302,7 @@ class Reservar_mesa:
         self.parte_inferior.columnconfigure(1, weight=1)
 
         #Etiqueta
-        Label(self.parte_superior, text="Reservar Mesa", font=("Calisto MT", 30, "bold"), bg=fondo).pack(side="top", pady=20)
+        Label(self.parte_superior, text="Reservar Mesa:", font=("Calisto MT", 30, "bold"), bg=fondo).pack(side="top", pady=20)
 
         #Imagen
         self.imagen = Image.open(r"reserva.png").resize((210, 220))
@@ -312,11 +315,9 @@ class Reservar_mesa:
         self.entry_nombre = Entry(self.parte_inferior, bd=0, width=14, font=("Arial", 16))
         self.entry_nombre .grid(row=0, column=1, columnspan=3, padx=5, sticky="w")
 
-        Label(self.parte_inferior, text="Cantidad de Personas:", font=("Arial", 16), bg=fondo).grid(row=1, column=0, pady=10, sticky="e")
-        self.entry_personas = Entry(self.parte_inferior, bd=0, width=14, font=("Arial", 16))
-        self.entry_personas.grid(row=1, column=1, columnspan=3, padx=5, sticky="w")
+        #Iria un label que diga el numero de mesa y cantidad de personas de la que es la mesa
 
-    #el primer boton es para guardar, llamaria a una funcion que guarda los datos y hace que cambie de color el boton
+         #el primer boton es para guardar, llamaria a una funcion que guarda los datos y hace que cambie de color el boton
         Button(self.parte_inferior, text="Guardar", width=16, font=("Arial", 12), command=lambda: self.regresar(ventana)).grid(row=2, column=0, columnspan=4, padx=35, pady=5)
 
         Button(self.parte_inferior, text="Atras", width=16, font=("Arial", 12), command=lambda: self.regresar(ventana)).grid(row=8, column=0, columnspan=4, padx=35, pady=5)
